9432a5c2cfff308ee7f897e42cf6216d76837941
dengshuibing <dengshuibing@DESKTOP-GGVE8V1>
2020-03-19 12:00:55 +0800

    改一改

--- src/helloword/HelloWorld.java
+++ src/helloword/HelloWorld.java
@@ -3,6 +3,6 @@ package helloword;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello worldËæ±ã¸ÄÒ»¸Ä");
 	}
 }
